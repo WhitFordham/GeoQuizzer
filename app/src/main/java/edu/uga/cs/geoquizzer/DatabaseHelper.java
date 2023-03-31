fae864de52ad480160fa3b07277dfe2ef254db6c
WhitFordham <dwfordham01@gmail.com>
2023-03-31 14:01:12 -0400

    Copied databse

--- app/src/main/java/edu/uga/cs/geoquizzer/DatabaseHelper.java
+++ app/src/main/java/edu/uga/cs/geoquizzer/DatabaseHelper.java
@@ -10,29 +10,10 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "quizInformation.db";
+    public static final String DATABASE_NAME = "quizInformation.db";
     private static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_LOCATION = "/data/data/edu.uga.cs.geoquizzer/databases";
     private static DatabaseHelper databaseInstance;
-    public static final String TABLE_COUNTRIES = "countries";
-    public static final String COUNTRIES_COLUMN_ID = "_id";
-    public static final String COUNTRIES_COLUMN_NAME = "name";
-    public static final String COUNTRIES_COLUMN_CONTINENT = "continent";
-
-    public static final String TABLE_QUIZZES = "quizzes";
-    public static final String QUIZ_COLUMN_ID = "_id";
-    public static final String QUIZ_COLUMN_DATE = "date";
-    public static final String QUIZ_COLUMN_RESULTS = "result";
-
-    public static final String CREATE_COUNTRIES = "create table " + TABLE_COUNTRIES + " ("
-            + COUNTRIES_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + COUNTRIES_COLUMN_NAME + "TEXT"
-            + COUNTRIES_COLUMN_CONTINENT + "TEXT" + ")";
-
-    public static final String CREATE_QUIZZES = "create table " + TABLE_QUIZZES + " ("
-            + QUIZ_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + QUIZ_COLUMN_DATE + " TEXT, "
-            + QUIZ_COLUMN_RESULTS + " TEXT" + ")";
-
     private DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -47,15 +28,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase database) {
-        database.execSQL(CREATE_COUNTRIES);
-        database.execSQL(CREATE_QUIZZES);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
-        database.execSQL("drop table if exists " + TABLE_COUNTRIES);
-        database.execSQL("drop table if exists " + TABLE_QUIZZES);
-        onCreate(database);
+
     }
 
     private void copyDatabase() {
--- app/src/main/java/edu/uga/cs/geoquizzer/MainActivity.java
+++ app/src/main/java/edu/uga/cs/geoquizzer/MainActivity.java
@@ -2,7 +2,14 @@ package edu.uga.cs.geoquizzer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseHelper helper = DatabaseHelper.getInstance(this);
+
+        File database = getApplicationContext().getDatabasePath(DatabaseHelper.DATABASE_NAME);
+
+        if (database.exists() == false) {
+            helper.getReadableDatabase();
+
+            if (copyDatabase(this)) {
+                Toast.makeText(this, "Successfully copied database", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Could not copy database", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+    }
+
+    private boolean copyDatabase(Context context) {
+        try {
+            InputStream inputStream = context.getAssets().open(DatabaseHelper.DATABASE_NAME);
+            String fileName = DatabaseHelper.DATABASE_LOCATION + DatabaseHelper.DATABASE_NAME;
+            OutputStream outputStream = new FileOutputStream(fileName);
+            byte [] buff = new byte[1024];
+            int length = 0;
+
+            while ((length = inputStream.read(buff)) > 0) {
+                outputStream.write(buff, 0, length);
+            }
+
+            outputStream.flush();
+            outputStream.close();
+
+            return true;
+        } catch (Exception error) {
+            error.printStackTrace();
+            return false;
+        }
     }
 }
