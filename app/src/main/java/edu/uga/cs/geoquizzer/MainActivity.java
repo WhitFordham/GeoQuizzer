e699bbdbf454208e0d37a76fed8343784f332be5
emilyzacharias <73804664+emilyzacharias@users.noreply.github.com>
2023-04-05 21:30:20 -0400

    Main Activity now displays splash screen.

--- app/src/main/java/edu/uga/cs/geoquizzer/MainActivity.java
+++ app/src/main/java/edu/uga/cs/geoquizzer/MainActivity.java
@@ -1,17 +1,38 @@
 package edu.uga.cs.geoquizzer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Calendar;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 public class MainActivity extends AppCompatActivity {
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment fragment = new SplashScreenFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace( R.id.fragmentContainerView, fragment).addToBackStack("main screen" ).commit();
     }
 }
